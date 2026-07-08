f267628bd478596e056d96410474d955dd3654ff
ronnywang <ronnywang@users.noreply.github.com>
2026-07-08 07:38:49 +0000

    deploy: bf59d722b123e884bc1e061c6fc419efe664a314

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 const api_base = 'https://pcc-api.openfun.app';
 const api_token = 'ofk_c925948ad0e7a5c6ed52db349bee0bf19ae4125c5c9aab87b4fec65d3e7747a3';
 
-$(function() {
-    if (api_token) {
-        $.ajaxSetup({
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader('Authorization', 'Bearer ' + api_token);
-            }
-        });
-    }
-});
+if (api_token) {
+    $.ajaxSetup({
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + api_token);
+        }
+    });
+}
